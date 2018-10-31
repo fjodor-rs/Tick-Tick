f15432e2d29d5368018e2c782cf51b8983ae9e6c
Joris <joris.l@hotmail.com>
2018-10-31 14:13:38 +0100

    button works kinda

--- TickTickFinal/menu/Button.cs
+++ TickTickFinal/menu/Button.cs
@@ -30,6 +30,6 @@ class Button : SpriteGameObject
 	public override void Update(GameTime gameTime)
 	{
 		if(GameEnvironment.GameStateManager.CurrentGameState == GameEnvironment.GameStateManager.GetGameState("playingState"))
-			position = new Vector2(Camera.Instance.Position.X * 2 + GraphicsDeviceManager.DefaultBackBufferWidth - Width + 10, 10);
-	}
+            position = new Vector2(Camera.Instance.Position.X + GraphicsDeviceManager.DefaultBackBufferWidth - 10, 10);
+    }
 }
