25347cf9e307a226e48a44d9fd9e5c20ef2ac26b
Joris L <Joris556@outlook.com>
2018-11-02 19:59:41 +0100

    aasdfghbgfdsdfvgbghfrdcvb nhgtff

--- TickTickFinal/gameobjects/Bomb.cs
+++ TickTickFinal/gameobjects/Bomb.cs
@@ -29,9 +29,18 @@ class Bomb : AnimatedGameObject
 		position.Y += verticalSpeed;
 		verticalSpeed += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 		PlayAnimation("celebrate");
-	}
+        //TileField tiles = GameWorld.Find("tiles") as TileField;
+        //Tile currentTile = tiles.Get((int)position.X, (int)position.Y) as Tile;
+        //TileType tileType = tiles.GetTileType((int)position.X, (int)position.Y);
+        //if (tileType != TileType.Background)
+        //{
+        //    PlayAnimation("explode");
+        //    velocity = Vector2.Zero;
+        //}
 
-	public override void Reset()
+    }
+
+    public override void Reset()
 	{
 		Velocity = Vector2.Zero;
 		gravity = 0;
